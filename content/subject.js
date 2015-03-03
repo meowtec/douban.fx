ef7cc759737a158ca7ce87aac4d291a144ef8344
花漫 <zhubo.zb@alibaba-inc.com>
2015-03-03 16:18:43 +0800

    专辑详情页面添加播放按钮

---
+++ content/subject.js
@@ -0,0 +1,12 @@
+var songs = [].slice.call(document.querySelectorAll('.song-items li'))
+
+songs.forEach(function (song) {
+  var playButton = _.node('<div class="col fx-play-button"><a target="_blank"></a></div>')
+
+  var play = playButton.children[0]
+  play.href = 'http://douban.fm/?start=' + song.id + 'g' + song.getAttribute('data-ssid') + 'g'
+
+  song.insertBefore(playButton, song.querySelector('.dl-count'))
+
+})
+
